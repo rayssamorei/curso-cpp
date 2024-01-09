2c0a3d4e1cebd5c156d1119ee0090da20bd8188a
rayssamorei <rayssa32alvess@gmail.com>
2024-01-09 20:30:47 -0300

    Exercicio aula 15 - Do while

---
+++ programas/Exercicio14.cpp
@@ -0,0 +1,23 @@
+//Aula 15 - exercicio sobre do while
+/*
+Desenvolva um programa que solicite ao usuário que insira uma senha. Use um loop do-while para continuar solicitando a senha até que ela corresponda a uma senha específica (por exemplo, "senha123").
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    string senhaCorreta = "senha123";
+    string senhaUsuario;
+
+    do{
+        cout << "Digite a senha: ";
+        cin >> senhaUsuario;
+
+        if(senhaUsuario != senhaCorreta){
+            cout << "Senha incorreta. Tente novamente. " << endl;
+        }
+    } while (senhaUsuario != senhaCorreta);
+
+    cout << "Senha correta! Acesso concedido. " << endl;
+    return 0;
+}
