26a089f47b1af602e5f0d4bfc02bbd7b12be8772
rayssamorei <rayssa32alvess@gmail.com>
2024-01-10 17:58:53 -0300

    Questão 06 - Cap04

---
+++ capitulo04/Questao07.cpp
@@ -0,0 +1,22 @@
+/*
+Uma empresa decide dar um aumento de 30% aos funcionários com salários inferiores a R$ 500,00. Faça um
+programa que receba o salário do funcionário e mostre o valor do salário reajustado ou uma mensagem, caso
+ele não tenha direito ao aumento.
+*/
+#include <iostream>
+using namespace std;
+
+int main (){
+    double salario, reajuste;
+
+    cout << "Informe seu salario: R$ ";
+    cin >> salario;
+
+    if(salario < 500){
+        reajuste = salario * 0.3;
+        cout << "O salario reajustado e: R$" << reajuste+salario;
+    } else {
+        cout << "Nao tem direito ao aumento. ";
+    }
+    return 0;
+}
