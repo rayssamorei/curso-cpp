eaf73150c42f27b89cc8ad1657d08e7d087ee8a1
rayssamorei <rayssa32alvess@gmail.com>
2024-01-10 18:26:15 -0300

    Questão 07 - Cap04

--- capitulo04/Questao07.cpp
+++ capitulo04/Questao07.cpp
@@ -2,6 +2,7 @@
 Uma empresa decide dar um aumento de 30% aos funcionários com salários inferiores a R$ 500,00. Faça um
 programa que receba o salário do funcionário e mostre o valor do salário reajustado ou uma mensagem, caso
 ele não tenha direito ao aumento.
+Questão 7
 */
 #include <iostream>
 using namespace std;
